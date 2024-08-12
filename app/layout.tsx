fba9dbe700735fe07f09105bb14f979515d765f9
GrowWithTalha <growwithtalha2@gmail.com>
2024-08-12 23:14:47 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Areeba Memon Portfolio",
+  description: "Graphic Designer Portfolio",
 };
 
 export default function RootLayout({
