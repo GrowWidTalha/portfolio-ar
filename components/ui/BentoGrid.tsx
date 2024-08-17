3fdc605645d8bc65497da905d65c4a0efdf93f6d
GrowWithTalha <growwithtalha2@gmail.com>
2024-08-17 14:19:16 +0500

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Canva", "Illustrator", "PhotoShop"];
+  const rightLists = ["Illustrator", "PhotoShop", "Canva"];
 
   const [copied, setCopied] = useState(false);
 
